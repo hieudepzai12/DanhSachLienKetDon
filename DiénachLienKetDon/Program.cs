16dc6f9fa88dfebc697a11218d4792986d17130a
hieudepzai12 <154107100+hieudepzai12@users.noreply.github.com>
2024-01-24 00:05:44 +0700

    Add files via upload

--- DiénachLienKetDon/Program.cs
+++ DiénachLienKetDon/Program.cs
@@ -90,6 +90,56 @@ namespace DiénachLienKetDon
                 p = p.Next;
             }
         }
+        public void Delete(int x)
+        {
+            if (Head != null)
+            {
+                Node p = Head;
+                Node q = null;
+
+                while (p!=null&& p.Info != x)
+                {
+                    q = p;
+                    p = p.Next;
+                }
+                if (p == Head)
+                {
+                    Deletehead();
+                }
+                else
+                {
+                    q.Next = p.Next;
+                    p.Next = null; 
+                }
+            }
+        }
+        public Node findMax()
+        {
+            Node max = Head;
+            Node p = Head.Next;
+            while (p != null)
+            {
+                if (p.Info > max.Info)
+                {
+                    max = p;
+                }
+                p = p.Next;
+            }
+            return max;
+        }
+        public float Avg()
+        {
+            float sum = 0;
+            int count = 0;
+            Node p = Head;
+            while (p != null)
+            {
+                sum += p.Info;
+                count++;
+                p = p.Next;
+            }
+            return sum / count;
+        }
     }
 
     class Program
@@ -97,20 +147,46 @@ namespace DiénachLienKetDon
         static void Main(string[] args)
         {
             SingleLinkList A = new SingleLinkList();
+            NhapDanhSach(A);
             Console.WriteLine("Dan sach lien ket la: ");
+
             A.AddHead(4);
             A.AddHead(6);
             A.AddHead(2);
             A.AddHead(12);
             A.processList();
-
+            int x;
             A.Deletehead();
             Console.WriteLine("\nDanh sach lien ket sau khi xoa nut dau: ");
             A.processList();
             A.DeleteLast();
             Console.WriteLine("\nDanh sach lien ket sau khi xoa nut cuoi: ");
             A.processList();
+            Console.WriteLine("\nXoa vi tri x: ");
+            x = int.Parse(Console.ReadLine());
+            A.Delete(x);
+            Console.WriteLine("Danh sach sau khi xoa:");
+            A.processList();
+
+            Node max = A.findMax();
+            Console.WriteLine($"\nNut co gia tri lon nhat: {max.Info}");
+
+            float tbc = A.Avg();
+            Console.WriteLine($"\nGia tri trung binh cac nut: {tbc}");
             Console.ReadKey();
+        }
+        static void NhapDanhSach(SingleLinkList A)
+        {
+            string chon = "y";
+            int x;
+            while (chon != "n")
+            {
+                Console.Write("Nhap gia tri nut n:");
+                x = int.Parse(Console.ReadLine());
+                A.AddHead(x);
+                Console.Write("Continue (y/n)?");
+                chon = Console.ReadLine();
+            }
         }
     }
 }
